5b4068ac9da3ec81960980ebdccc0901bb636c70
Alsu3912 <alsu3912@gmail.com>
2021-01-21 17:10:12 +0000

    React Router

--- src/component/MainComponent.js
+++ src/component/MainComponent.js
@@ -1,28 +1,41 @@
 import React, { useState } from 'react';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent'; 
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 function Main() {
   const [dishes, setDishes] = useState(DISHES);
-  const [selectedDish, setSelectedDish] = useState(null);
+  // const [selectedDish, setSelectedDish] = useState(null);
 
-  const onDishSelect = (dishID) => {
-    setSelectedDish(dishID);
-  }
+  // const onDishSelect = (dishID) => {
+  //   setSelectedDish(dishID);
+  // }
 
-  const filterByDishID = () => {
-    const filteredArrayOfDishes = dishes.filter((dish) => dish.id === selectedDish);
-    return filteredArrayOfDishes[0];
+  // const filterByDishID = () => {
+  //   const filteredArrayOfDishes = dishes.filter((dish) => dish.id === selectedDish);
+  //   return filteredArrayOfDishes[0];
+  // }
+
+  const HomePage = () => {
+    return (
+      <Home />
+    )
   }
 
   return (
     <div>
       <Header />
-      <Menu dishes={dishes} onClick={(dishID) => onDishSelect(dishID)}/>
-      <DishDetail dish={filterByDishID(selectedDish)} />
+      <Switch>
+        <Route path="/home" component={HomePage}/>
+        <Route exact path="/menu" component={() => <Menu dishes={dishes}/>}/>
+        <Redirect to="/home"></Redirect>
+      </Switch>
+      {/* <Menu dishes={dishes} onClick={(dishID) => onDishSelect(dishID)}/>
+      <DishDetail dish={filterByDishID(selectedDish)} /> */}
       <Footer />
     </div>
   );
