63a9d58b5c0371a238c87d6cd555311632a77775
Alsu3912 <alsu3912@gmail.com>
2021-01-22 16:47:14 +0000

    Single Page Applications Part 1

--- src/component/MainComponent.js
+++ src/component/MainComponent.js
@@ -1,14 +1,21 @@
 import React, { useState } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
+import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent'; 
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 function Main() {
   const [dishes, setDishes] = useState(DISHES);
+  const [comments, setComments] = useState(COMMENTS);
+  const [leaders, setLeaders] = useState(LEADERS);
+  const [promotions, setPromotions] = useState(PROMOTIONS);
   // const [selectedDish, setSelectedDish] = useState(null);
 
   // const onDishSelect = (dishID) => {
@@ -20,9 +27,15 @@ function Main() {
   //   return filteredArrayOfDishes[0];
   // }
 
+  const filterByFeaturedAttribute = (sortableArray) => {
+    const filteredArray = sortableArray.filter((elem) => elem.featured);
+    return filteredArray[0];
+  }
+
   const HomePage = () => {
     return (
-      <Home />
+      <Home dish={filterByFeaturedAttribute(dishes)} promotion={filterByFeaturedAttribute(promotions)} 
+      leader={filterByFeaturedAttribute(leaders)} />
     )
   }
 
@@ -31,7 +44,8 @@ function Main() {
       <Header />
       <Switch>
         <Route path="/home" component={HomePage}/>
-        <Route exact path="/menu" component={() => <Menu dishes={dishes}/>}/>
+        <Route exact path="/menu" component={() => <Menu dishes={dishes} />}/>
+        <Route exact path="/contactus" component={Contact} />
         <Redirect to="/home"></Redirect>
       </Switch>
       {/* <Menu dishes={dishes} onClick={(dishID) => onDishSelect(dishID)}/>
