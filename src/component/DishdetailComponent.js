cb09c532325411e067abfda8c27c52c9cff0b5ab
Alsu3912 <alsu3912@gmail.com>
2021-02-17 20:16:58 +0000

    Fetch from Server

--- src/component/DishdetailComponent.js
+++ src/component/DishdetailComponent.js
@@ -6,13 +6,14 @@ import {
 import { Link } from 'react-router-dom';
 import CommentForm from './CommentForm';
 import Loading from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 function RenderDish({ dish }) {
   if (dish != null)
     return (
       <div className="col-12 col-md-5 m-1">
         <Card>
-          <CardImg top src={dish.image} alt={dish.name} />
+          <CardImg top src={baseUrl + dish.image} alt={dish.name} />
           <CardBody>
             <CardTitle>{dish.name}</CardTitle>
             <CardText>{dish.description}</CardText>
@@ -57,7 +58,7 @@ function RenderComments({ comments, dishId }) {
     );
 }
 
-function DishDetail({ dish, isLoading, errorMessage, comments }) {
+function DishDetail({ dish, isLoading, errorMessage, comments, commentsErrorMessage }) {
   if (isLoading) {
     return (
       <div className="container">
